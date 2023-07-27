a4edefc39a787c8699e989926a8081199d48363f
Pedrocaloi <caloieropedro@gmail.com>
2023-07-27 11:30:09 -0300

    boton en ingles

--- client/src/app/components/Home.js
+++ client/src/app/components/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
          to='portfolio'
          smooth
          duration={500}
-         className='group text-white w-fit px-6 py-4 my-2 flex items-center rounded-md bg-gradient-to-r from-violet-900 to-orange-700 cursor-pointer'>
+         className='group text-white w-fit px-6 py-4 my-10 flex items-center rounded-md bg-gradient-to-r from-violet-900 to-orange-700 cursor-pointer'>
          Portfolio
          <span className='group-hover:rotate-90 duration-300'>
           <MdOutlineKeyboardArrowRight
